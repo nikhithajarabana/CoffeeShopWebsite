13e4d73e2c4ee8434a9c0bde93d47f648691305e
rithvik <vanteru.r@northeastern.edu>
2023-04-21 21:44:39 -0400

    final validations

--- INFO6150_WebDesign_FinalProject_Client-main/Server/models/userModel.js
+++ INFO6150_WebDesign_FinalProject_Client-main/Server/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose")  //to import mongoose package
+const bcrypt = require("bcrypt");
+const saltRounds = 10; // the number of salt rounds to use for hashing
 
 
 const userSchema = mongoose.Schema({   //creating user schema
@@ -10,5 +12,20 @@ const userSchema = mongoose.Schema({   //creating user schema
     timestamps: true,
 })
 
+// hash the password before saving it to the database
+userSchema.pre("save", async function (next) {
+    const user = this;
+    if (!user.isModified("password")) {
+      return next();
+    }
+    const hash = await bcrypt.hash(user.password, saltRounds);
+    user.password = hash;
+    next();
+  });
+  
+  // compare the user input password with the hashed password in the database
+  userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+  };
 
 module.exports = mongoose.model('users', userSchema) //compiling user schema into the model and exporting it to be used into routes files
